24b9d41e96f4ea05375778f49006f4947704769e
David Ralph <ohlookitsderpy@protonmail.com>
2019-05-05 09:33:30 +0100

    Update Title.jsx

--- Components/Title.jsx
+++ Components/Title.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Title extends React.Component{
     render(){
         return (
-          <div>
+          <div style={{textAlign: 'center'}}>
             <h1>Hello Space!</h1>
             <h2>I'm David Ralph, a fullstack developer from the United Kingdom</h2>
           </div>
