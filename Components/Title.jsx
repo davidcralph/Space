13207721d90dffd05a2f35e3c5906a4d21b073de
David Ralph <ohlookitsderpy@protonmail.com>
2019-05-05 09:33:48 +0100

    Update Title.jsx

--- Components/Title.jsx
+++ Components/Title.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-class Title extends React.Component{
+class Title extends React.Component {
     render(){
         return (
           <div style={{textAlign: 'center'}}>
